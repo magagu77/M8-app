ddd9900b996816375fefb570dc630ee3c74c0eef
Marc <marc.garcia0502@gmail.com>
2022-11-26 10:26:52 +0100

    Añadida informacion en la vista del fragment detail

--- app/src/main/java/com/m6/pruebas/fragments/DetailFragment.kt
+++ app/src/main/java/com/m6/pruebas/fragments/DetailFragment.kt
@@ -4,18 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.m6.pruebas.Pokemon
 import com.m6.pruebas.R
 
 
 class DetailFragment(pokemon:Pokemon) : Fragment() {
-
+    var pk:Pokemon = pokemon
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        val view: View =  inflater.inflate(R.layout.fragment_detail, container, false);
+
+        val txtDetail: TextView = view.findViewById(R.id.textDetail);
+        val txt:String = "${pk.getName()} es un pokemon"
+        txtDetail.setText(txt)
+
+        return view;
+
     }
 }
