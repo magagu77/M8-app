0b2291128e6173ca028826390d0943397ec4ca48
Marc <marc.garcia0502@gmail.com>
2022-12-05 19:40:10 +0100

    Añadido boton shared preferences

--- app/src/main/java/com/m6/pruebas/MainActivity.kt
+++ app/src/main/java/com/m6/pruebas/MainActivity.kt
@@ -1,34 +1,43 @@
 package com.m6.pruebas
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.m6.pruebas.DB.PokemonDBHelper
 
 class MainActivity : AppCompatActivity() {
-
-    companion object {
-        lateinit var dbHelper:PokemonDBHelper
-    }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var btnSignIn: Button = findViewById(R.id.btnSignIn);
-        var txtUsername:EditText = findViewById(R.id.txtUsername);
-        var txtPassword: EditText = findViewById(R.id.txtPassword);
-
+        val sharedPreference =  getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
+        val user = sharedPreference.getString("user","");
+        val pass = sharedPreference.getString("password","")
+        val def = sharedPreference.getBoolean("default", false)
+        val btnSignIn: Button = findViewById(R.id.btnSignIn);
+        val txtUsername:EditText = findViewById(R.id.txtUsername);
+        txtUsername.setText(user)
+        val txtPassword: EditText = findViewById(R.id.txtPassword);
+        txtPassword.setText(pass)
+        val remember: CheckBox = findViewById(R.id.checkBox)
+        remember.isChecked = def
+        val editor = sharedPreference.edit()
         btnSignIn.setOnClickListener {
-            val userName: String = "1";
-            val password:String = "1";
-            if (userName.equals("1") && password.equals("1")){
-            //if(txtUsername.getText().toString() == "1" && txtPassword.getText().toString()=="1"){
-                Log.i("testMarc", "Login correcte" + userName);
-
+            val userName: String = txtUsername.getText().toString();
+            val password:String = txtPassword.getText().toString();
+            if (userName.equals("admin") && password.equals("admin")){
+                Log.i("testMarc", "Login correcte" + userName)
+                editor.clear().commit();
+                if(remember.isChecked()) {
+                    editor.putString("user", userName);
+                    editor.putString("password", password)
+                    editor.putBoolean("default", true)
+                    editor.commit()
+                }
                 var intent = Intent(this, BottomNavigation::class.java);
                 startActivity(intent);
             }else{
